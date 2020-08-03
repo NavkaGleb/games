d70a6511cd56ff5c7cf5148cbc4a4e8760182a3d
Gleb Navka <navka.gleb@gmail.com>
2020-08-03 15:55:09 +0300

    implement StatusBar

--- Snake/src/GUI/StatusBar.cpp
+++ Snake/src/GUI/StatusBar.cpp
@@ -1,4 +1,54 @@
-//
-// Created by navka on 8/3/2020.
-//
+#include "StatusBar.hpp"
 
+#include <cmath>
+
+namespace ng {
+
+	// constructor / destructor
+	StatusBar::StatusBar(float x, float y, float width, float height, sf::Color backgroundColor, sf::Color statusColor)
+		: _active(false) {
+
+		// init background
+		this->_background.setPosition(x, y);
+		this->_background.setSize(sf::Vector2f(width, height));
+		this->_background.setFillColor(backgroundColor);
+
+		// init status
+		this->_status.setPosition(x, y);
+		this->_status.setSize(sf::Vector2f(width, height));
+		this->_status.setFillColor(statusColor);
+
+	}
+
+	StatusBar::~StatusBar() {
+
+
+
+	}
+
+	// accessors
+	const bool& StatusBar::active() const { return this->_active; }
+
+	// modifiers
+	void StatusBar::active(const bool& active) { this->_active = active; }
+
+	// public methods
+	void StatusBar::update(const float& percentage) {
+
+		this->_status.setSize(sf::Vector2f(
+			std::floor(this->_background.getGlobalBounds().width * percentage),
+			this->_status.getGlobalBounds().height
+		));
+
+	}
+
+	void StatusBar::render(sf::RenderTarget& target) {
+
+		target.draw(this->_background);
+		target.draw(this->_status);
+
+	}
+
+	// private methods
+
+} // namespace ng
