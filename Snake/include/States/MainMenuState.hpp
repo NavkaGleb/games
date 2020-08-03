2b7f4c660baed99847613f1b7ff04ff1181232b9
Gleb Navka <navka.gleb@gmail.com>
2020-08-03 17:29:15 +0300

    add MainMenuState class

---
+++ Snake/include/States/MainMenuState.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace ng {
+
+	class MainMenuState {
+	public:
+		// constructor / destructor
+		
+		// public methods
+
+	private:
+		// variables
+
+		// private methods
+
+	}; // class MainMenuState
+
+} // namespace ng
